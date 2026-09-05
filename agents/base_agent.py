018fa2f9dfa914c326feafcc4c35c273803a0b18
yzccc <yangzc2004@gmail.com>
2026-09-05 21:18:00 +0800

    feat: add base_agent stub

---
+++ agents/base_agent.py
@@ -0,0 +1,4 @@
+"""Agent 基类：状态(position/energy/carrying_food/memory/communication_history/role)、
+局部感知(nearby_food/nearby_agents/pheromone_signal/environment_change)与动作空间
+(explore/collect_food/return_home/communicate/help/rest)。对应设计文档第 5 节。
+"""
